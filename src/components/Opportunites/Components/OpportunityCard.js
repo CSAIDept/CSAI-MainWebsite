d9aeb2973041b7234425c8448891976f4f1b921d
Shubham <tiwarishubham635@gmail.com>
2020-12-06 20:49:51 +0530

    Scraper added and Opportunities updated

--- src/components/Opportunites/Components/OpportunityCard.js
+++ src/components/Opportunites/Components/OpportunityCard.js
@@ -2,11 +2,18 @@ import React from 'react';
 
 function OpportunityCard(props){
     return(
-        <div className="Opportunity-Content-elements">
-                <h2>{props.item.name}</h2>
-                <br/>
-                <p>{props.item.description}</p>
+        <div className='Opportunities-Content'>
+            <div className='Opportunity-Content-heading'>
+            <h2>{props.item.name}</h2>
             </div>
+            <br/>
+            <br/>
+            <div className="Opportunity-Content-elements">
+                
+                <iframe width="300" height="200" src={props.item.video} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <p>{props.item.description}<a href={props.item.link}>{props.item.link}</a></p>
+            </div>
+        </div>
     )
 }
 
